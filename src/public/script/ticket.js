b4f865c8b9bbba45cc8e4880d79008906ec3be6d
dqbinh20 <quangbinh572@gmail.com>
2023-11-01 18:05:17 +0700

    page ticket - progress 1 (choose eseat)

--- src/public/script/ticket.js
+++ src/public/script/ticket.js
@@ -1,4 +1,27 @@
-const btnNext = document.getElementById("btn-next");
-const progressBar = document.getElementsByClassName("progress-bar")[0];
+const seats = document.querySelectorAll(".seat:not(.booked)");
+seats.forEach((seat) => {
+  seat.addEventListener("click", () => {
+    seat.classList.toggle("selected");
+  });
+});
 
-btnNext.addEventListener("click", () => {});
+const bookButton = document.getElementById("book-button");
+bookButton.addEventListener("click", function () {
+  var selectedSeat = document.querySelectorAll(".seat.selected");
+  if (selectedSeat.length == 0) {
+    alert("Please choose one");
+  } else {
+    selectedSeat.forEach((seat) => {
+      console.log(seat.dataset);
+    });
+  }
+});
+
+// const progressBar = document.getElementById("progress-bar");
+// const progressFirst = document.getElementById("progess-first");
+// const progressSecond = document.getElementById("progess-second");
+// const progressThird = document.getElementById("progess-third");
+
+// btnNext.addEventListener("click", () => {
+//   // if (currentProgress == 1)
+// });
